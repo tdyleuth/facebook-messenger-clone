c68ff1e2874c7a906aeefe4baea3d9f2589af094
tdyleuth <tony.dyleuth@gmail.com>
2020-09-10 17:21:51 -0700

    Updated App.css

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Message from "./Message";
 import db from "./firebase";
 import firebase from "firebase";
 import FlipMove from "react-flip-move";
+import SendIcon from "@material-ui/icons/Send";
+import { IconButton } from "@material-ui/core";
 
 function App() {
     const [input, setInput] = useState("");
@@ -45,24 +47,33 @@ function App() {
 
     return (
         <div className='App'>
+            <img
+                className='fb-messenger-icon'
+                alt='facebook messenger icon'
+                src='https://cdn.iconscout.com/icon/free/png-512/facebook-messenger-2-569346.png'
+            />
+
             <h1>Facebook Messenger</h1>
             <h2>Welcome {username}</h2>
-            <form>
-                <FormControl>
-                    <InputLabel>Enter a message...</InputLabel>
+            <form className='app-form'>
+                <FormControl className='app-formControl'>
                     <Input
+                        className='app-input'
+                        placeholder='Enter a message..'
                         value={input}
                         onChange={(event) => setInput(event.target.value)}
                     />
-                    <Button
+
+                    <IconButton
+                        className='app_iconButton'
                         disabled={!input}
                         variant='contained'
                         color='primary'
                         type='submit'
                         onClick={sendMessage}
                     >
-                        Send Message
-                    </Button>
+                        <SendIcon />
+                    </IconButton>
                 </FormControl>
             </form>
 
--- src/Message.js
+++ src/Message.js
@@ -4,12 +4,14 @@ import "./Message.css";
 
 const Message = forwardRef(({ message, username }, ref) => {
     const isUser = username === message.username;
+
     return (
         <div ref={ref} className={`message ${isUser && "message-user"}`}>
             <Card className={isUser ? "message-userCard" : "message-guestCard"}>
                 <CardContent>
                     <Typography color='white' variant='h5' component='h2'>
-                        {message.username}: {message.message}
+                        {!isUser && `${message.username || "Unknown User"}: `}{" "}
+                        {message.message}
                     </Typography>
                 </CardContent>
             </Card>
